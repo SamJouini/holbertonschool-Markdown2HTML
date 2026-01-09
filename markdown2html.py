99b951ff27206422a63eaf40ebd24cac4f3b3a87
SamJouini <samantha.u.h.jouini@gmail.com>
2026-01-09 10:55:20 +0100

    requirement compliance

--- markdown2html.py
+++ markdown2html.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
+"""
+markdown2html.py
+
+Checks command-line arguments and validates the existence
+of a Markdown file before HTML conversion.
+"""
 
 import sys
 import os
 
-if len(sys.argv)<3: 
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
 
-markdown_file=sys.argv[1]
+def main():
+    """Function that validates arguments and input file."""
+    if len(sys.argv) < 3:
+        print(
+            "Usage: ./markdown2html.py README.md README.html",
+            file=sys.stderr
+        )
+        sys.exit(1)
 
-if not os.path.exists(markdown_file):
-    print(f"Missing {markdown_file}", file=sys.stderr)
-    sys.exit(1)
+    markdown_file = sys.argv[1]
 
-sys.exit(0)
+    if not os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
